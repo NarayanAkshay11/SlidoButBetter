97831af94caabfbb0bb613617f241d56ddbe4528
Akshay Narayanan B <90465549+NarayanAkshay11@users.noreply.github.com>
2024-02-21 13:53:44 +0800

    Update script.js for proper recording

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function updateText() {
   var inputText = document.getElementById('textInput').value;
-  var displayContainer = document.getElementById('displayContainer');
-  var newBox = document.createElement('div');
-  newBox.className = 'box';
-  newBox.textContent = inputText;
-  displayContainer.appendChild(newBox);
-  document.getElementById('textInput').value = ''; // Clear input field after adding text
+  if (inputText.trim() !== '') {
+    var displayContainer = document.getElementById('displayContainer');
+    var newBox = document.createElement('div');
+    newBox.className = 'box';
+    newBox.textContent = inputText;
+    displayContainer.appendChild(newBox);
+    document.getElementById('textInput').value = ''; // Clear input field after adding text
+  }
 }
-
-// Update text every 5 seconds (5000 milliseconds)
-setInterval(updateText, 5000);
